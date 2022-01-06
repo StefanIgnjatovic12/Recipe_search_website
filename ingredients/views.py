29ae01cd788ea94e88f075cc1e9d169d1ff1dcd7
Stefan <sper1234@gmail.com>
2022-01-06 20:33:10 +0100

    Made API calls work, trying to figure out how to create multiple objects from the data. Will check serializers tomorrow

--- ingredients/views.py
+++ ingredients/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.generic import (ListView)
 from ingredients.models.modelsIngredients import Ingredients, FoodGroups
 from ingredients.models.modelSelected import SelectedIngredients
+from ingredients.models.modelsRecipe import DisplayRecipe, FavoriteRecipe
 from .forms import IngredientButton
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
@@ -57,19 +58,58 @@ def post(request):
 def search_for_recipe(request):
     if(request.GET.get('mybtn')):
         ing_list = []
+
         for item in SelectedIngredients.objects.all():
             ing_list.append(item.food)
+
         api_key = '70b0e02384834d1db2b66fb35bd97984'
         params = {
             'apiKey':api_key,
             'number':3,
             'ingredients':ing_list
         }
+
         response = requests.get(url='https://api.spoonacular.com/recipes/findByIngredients', params=params)
         response.raise_for_status()
-        # with open('testapireturn.json', 'w') as fp:
-        #     json.dump(response.json(), fp)
-        recipes = json.loads(response.content)
+        recipes1 = json.loads(response.content)
+
+        # first API Call just gets ID and ingredient list, have to use the ID for a second API call to get detailed info
+        # Append all the IDs to a list and then use it as a parameter
+        id_list = []
+        for recipe in recipes1:
+            id_list.append(str(recipe['id']))
+
+        params2 =  {
+            'apiKey':api_key,
+            'ids':",".join(id_list),
+            'includeNutrition': True
+        }
+        response2 = requests.get(url='https://api.spoonacular.com/recipes/informationBulk', params=params2)
+        response2.raise_for_status()
+        recipes2 = json.loads(response2.content)
+
         # generate recipes objects using the json data upon API call
-        return render(request, 'ingredients/apitest.html', {'recipes': recipes})
+        obj = DisplayRecipe()
+        for recipe2 in recipes2:
+            obj.title = recipe2['title']
+            obj.link = recipe2['sourceUrl']
+            obj.img = recipe2['image']
+            obj.calories = recipe2['nutrition']['nutrients'][0]['amount']
+            obj.fat = recipe2['nutrition']['nutrients'][1]['amount']
+            obj.saturated_fat = recipe2['nutrition']['nutrients'][2]['amount']
+            obj.carbs = recipe2['nutrition']['nutrients'][3]['amount']
+            obj.protein = recipe2['nutrition']['nutrients'][8]['amount']
+            obj.sodium = recipe2['nutrition']['nutrients'][7]['amount']
+            obj.sugar = recipe2['nutrition']['nutrients'][5]['amount']
+            # obj.ingredients = (
+            #     for num in range(len(recipe2['nutrition']['ingredients'])):
+            #         for ingredient in recipe2['nutrition']['ingredients'][num]:
+            #
+            # )
+            print(obj.title)
+            obj.save()
+            print(DisplayRecipe.objects.all())
+        print(DisplayRecipe.objects.all())
+        return render(request, 'ingredients/apitest.html', {'recipes': recipes2})
     return render(request, 'ingredients/apitest.html')
+
