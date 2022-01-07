e412e23cd2030dce18d2531a166ef377bb41b96a
Stefan <sper1234@gmail.com>
2022-01-07 20:28:19 +0100

    Added modelsRecipe file

---
+++ ingredients/models/modelsRecipe.py
@@ -0,0 +1,28 @@
+from django.db import models
+class DisplayRecipe (models.Model):
+    title = models.TextField(max_length=500)
+    ingredients = models.TextField(max_length=500)
+    link = models.URLField()
+    img = models.URLField()
+    calories = models.TextField(max_length=500)
+    fat = models.TextField(max_length=500)
+    carbs = models.TextField(max_length=500)
+    protein = models.TextField(max_length=500)
+    cholesterol = models.TextField(max_length=500)
+    sodium = models.TextField(max_length=500)
+    saturated_fat = models.TextField(max_length=500)
+    sugar = models.TextField(max_length=500)
+
+class FavoriteRecipe (models.Model):
+    title = models.TextField(max_length=500)
+    ingredients = models.TextField(max_length=500)
+    link = models.URLField()
+    img = models.URLField()
+    calories = models.TextField(max_length=500)
+    fat = models.TextField(max_length=500)
+    carbs = models.TextField(max_length=500)
+    protein = models.TextField(max_length=500)
+    cholesterol = models.TextField(max_length=500)
+    sodium = models.TextField(max_length=500)
+    saturated_fat = models.TextField(max_length=500)
+    sugar = models.TextField(max_length=500)
